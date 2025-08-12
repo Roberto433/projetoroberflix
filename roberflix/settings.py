e4c8dade4ab4d465fb0dde88a7fea7f0ef4f7b85
Roberto433 <rfc971510235@gmail.com>
2025-08-12 20:37:34 -0300

    ajuste banco de dados

--- roberflix/settings.py
+++ roberflix/settings.py
@@ -87,20 +87,19 @@ WSGI_APPLICATION = "roberflix.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 import dj_database_url
-
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
 
 # Password validation
