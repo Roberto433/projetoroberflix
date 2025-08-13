00097ec0cbb84c479aaa09e30eba6ad781e16682
Roberto433 <rfc971510235@gmail.com>
2025-08-13 11:20:43 -0300

    revisao banco de dados

--- roberflix/settings.py
+++ roberflix/settings.py
@@ -98,7 +98,7 @@ import dj_database_url
 DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL:
     DATABASE = {
-        'defaut': dj_database_url.config(default=DATABASE_URL,conn_max_age=600, conn_health_checks=True)
+        "default": dj_database_url.config(default='DATABASE_URL',conn_max_age=600, conn_health_checks=True)
     }
 
 
